2103edb1dfc2ca7b7aed0b7e653994d51edce6d3
Dhivyeshgolam15 <golamyashveer@gmail.com>
2023-04-25 15:28:14 +0400

    add docs

---
+++ WEEK2/Exercises/2/index.js
@@ -0,0 +1,12 @@
+const newDog = "Chihuahua"
+
+console.log("number of letters in newDog: ", newDog.length)
+
+console.log(newDog.toUpperCase)
+console.log(newDog.toLowerCase)
+
+if (newDog === "Chihuahua") {
+    console.log("I love Chihuhua, Uta my favourite doa breed")
+    {esle}
+    console.log("I dont care , i prefer cats")
+}
