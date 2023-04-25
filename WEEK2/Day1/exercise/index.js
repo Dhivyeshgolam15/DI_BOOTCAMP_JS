08e96910ee3ece5c0df1037cde5e4e6cad1d7864
Dhivyeshgolam15 <golamyashveer@gmail.com>
2023-04-25 18:02:22 +0400

    add doc

---
+++ WEEK2/Day1/exercise/index.js
@@ -0,0 +1,12 @@
+//Store your favorite food into a variable.
+const favoriteFood = "apples"
+
+//Store your favorite meal of the day into a variable (ie. breakfast, lunch or dinner)
+const favoriteMeal = "Breakfast"
+//Console.log I eat <favorite food> at every <favorite meal>
+
+// STRING CONCATENATION
+console.log("I eat " + favoriteFood + "at every" + favoriteMeal)
+
+//TEMPLATE LITERAL
+console.log("I eat {favoriteFood} at every ${favoriteMeal}")
---
+++ WEEK2/Day1/exercise2/index.js
@@ -0,0 +1,20 @@
+//Using this array : 
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+//Create a variable named myWatchedSeriesLength that is equal to the number of series in the myWatchedSeries array.
+const myWatchedSeriesLength = myWatchedSeriesLength
+
+//Create a variable named myWatchedSeriesSentence, that is equal to a sentence describing the series you watched
+//For example : black mirror, money heist, and the big bang theory
+
+//CONCATENATION
+//CONST mywatchedseriesSentence + mywatchedseries[0]"," + mywatchedseries[1]" and " + mywatchedseies[2]
+
+//TEMPLATE LITERAL
+const mywatchedseriesSentence = "${mywatchedSeries[0]}, ${myWatchedSeries[1]} and ${mywatchedseries[2]}"
+
+//Console.log a sentence using both of the variables created above
+//For example : I watched 3 series : black mirror, money heist, and the big bang theory
+
+const sentence = "I watched ${mywatchedserieslength} series: ${mywatchedseriessentence}"
+console.log(sentence)
