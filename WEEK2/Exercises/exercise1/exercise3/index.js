bde2b6fd219b4bd4ee0758872349cc8018a617c3
Dhivyeshgolam15 <golamyashveer@gmail.com>
2023-04-25 16:14:21 +0400

    add exercise

---
+++ WEEK2/Exercises/exercise1/exercise3/index.js
@@ -0,0 +1,7 @@
+const usernumber = prompt("enter a nunmber")
+
+if (Number(usernumber) % 2 === 0){
+    console.log(usernumber + "is an even number")
+}else {
+        console.log(usernumber + "is an odd number")
+}
---
+++ WEEK2/Exercises/exercise1/exercise4/index.js
@@ -0,0 +1,9 @@
+const users = [];
+
+if(users.length === 0 ) console.log("no one is online")
+
+if(users.length === 1 ) console.log(user[0] + "are online")
+
+if(users.length === 2 ) (console.log(users[0]) + "and" + users [1] + " are online")
+
+if (users.length > 2) console.log("$(users[0]"), $(users[1]) , $(users.length - 2); "more are online"
